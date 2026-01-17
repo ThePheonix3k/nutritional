03212a0d938f0d434bf56adc8748b1c684942674
ThePheonix3k <101991744+ThePheonix3k@users.noreply.github.com>
2026-01-17 07:53:42 -0500

    inital commit

---
+++ src/main/java/com/ThePheonix3k/nutritional/Blocks/ModBlocks.java
@@ -0,0 +1,32 @@
+package com.ThePheonix3k.nutritional.Blocks;
+
+import com.ThePheonix3k.nutritional.Nutritional;
+import com.ThePheonix3k.nutritional.blockentites.FarmlandBlockEntity;
+import net.minecraft.block.*;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+    public static void initialize() {}
+
+    public static Block register(Block block, String name, boolean shouldRegisterItem) {
+        Identifier id = new Identifier(Nutritional.MOD_ID, name);
+
+        if (shouldRegisterItem) {
+            BlockItem blockItem = new BlockItem(block, new Item.Settings());
+            Registry.register(Registries.ITEM, id, blockItem);
+        }
+
+        return Registry.register(Registries.BLOCK, id, block);
+    }
+
+    public static final Block NUTRITIONAL_FARMLAND = register(
+            new NutritionalFarmlandBlock(AbstractBlock.Settings.create().mapColor(MapColor.DIRT_BROWN).ticksRandomly().strength(0.6f).sounds(BlockSoundGroup.GRAVEL).blockVision(Blocks::always).suffocates(Blocks::always)),
+            "farmland",
+            true
+    );
+}
