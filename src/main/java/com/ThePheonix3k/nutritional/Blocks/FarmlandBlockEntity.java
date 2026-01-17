20321b23b9ca497039959f97fd1b0029b4cc7b76
ThePheonix3k <101991744+ThePheonix3k@users.noreply.github.com>
2026-01-17 16:42:01 -0500

    turned farmlandblock into block entity

--- src/main/java/com/ThePheonix3k/nutritional/Blocks/FarmlandBlockEntity.java
+++ src/main/java/com/ThePheonix3k/nutritional/Blocks/FarmlandBlockEntity.java
@@ -1,19 +1,134 @@
 package com.ThePheonix3k.nutritional.Blocks;
 
-import net.minecraft.block.BlockEntityProvider;
-import net.minecraft.block.BlockState;
+import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.pathing.NavigationType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.FluidTags;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.state.property.Property;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.*;
+import net.minecraft.world.event.GameEvent;
 import org.jetbrains.annotations.Nullable;
 
-public class FarmlandBlockEntity extends BlockEntity implements BlockEntityProvider {
-    public FarmlandBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
-        super(type, pos, state);
+public class FarmlandBlockEntity extends BlockWithEntity implements BlockEntityProvider {
+    protected FarmlandBlockEntity(Settings settings) {
+        super(settings);
+    }
+
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
     }
 
     @Override
     public @Nullable BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
         return null;
+    }
+
+    public static final IntProperty MOISTURE;
+    protected static final VoxelShape SHAPE;
+
+
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if (direction == Direction.UP && !state.canPlaceAt(world, pos)) {
+            world.scheduleBlockTick(pos, this, 1);
+        }
+
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+    }
+
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        BlockState blockState = world.getBlockState(pos.up());
+        return !blockState.isSolid() || blockState.getBlock() instanceof FenceGateBlock || blockState.getBlock() instanceof PistonExtensionBlock;
+    }
+
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return !this.getDefaultState().canPlaceAt(ctx.getWorld(), ctx.getBlockPos()) ? Blocks.DIRT.getDefaultState() : super.getPlacementState(ctx);
+    }
+
+    public boolean hasSidedTransparency(BlockState state) {
+        return true;
+    }
+
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
+    }
+
+    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if (!state.canPlaceAt(world, pos)) {
+            setToDirt((Entity)null, state, world, pos);
+        }
+
+    }
+
+//    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+//        int i = (Integer)state.get(MOISTURE);
+//        if (!isWaterNearby(world, pos) && !world.hasRain(pos.up())) {
+//            if (i > 0) {
+//                world.setBlockState(pos, (BlockState)state.with(MOISTURE, i - 1), 2);
+//            } else if (!hasCrop(world, pos)) {
+//                setToDirt((Entity)null, state, world, pos);
+//            }
+//        } else if (i < 7) {
+//            world.setBlockState(pos, (BlockState)state.with(MOISTURE, 7), 2);
+//        }
+//
+//    }
+
+
+    //i HATE crop trampling. All my homies HATE crop trampling.
+
+//    public void onLandedUpon(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance) {
+//        if (!world.isClient && world.random.nextFloat() < fallDistance - 0.5F && entity instanceof LivingEntity && (entity instanceof PlayerEntity || world.getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING)) && entity.getWidth() * entity.getWidth() * entity.getHeight() > 0.512F) {
+//            setToDirt(entity, state, world, pos);
+//        }
+//
+//        super.onLandedUpon(world, state, pos, entity, fallDistance);
+//    }
+
+    public static void setToDirt(@Nullable Entity entity, BlockState state, World world, BlockPos pos) {
+        BlockState blockState = pushEntitiesUpBeforeBlockChange(state, Blocks.DIRT.getDefaultState(), world, pos);
+        world.setBlockState(pos, blockState);
+        world.emitGameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Emitter.of(entity, blockState));
+    }
+
+    private static boolean hasCrop(BlockView world, BlockPos pos) {
+        return world.getBlockState(pos.up()).isIn(BlockTags.MAINTAINS_FARMLAND);
+    }
+
+    private static boolean isWaterNearby(WorldView world, BlockPos pos) {
+        for(BlockPos blockPos : BlockPos.iterate(pos.add(-4, 0, -4), pos.add(4, 1, 4))) {
+            if (world.getFluidState(blockPos).isIn(FluidTags.WATER)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(new Property[]{MOISTURE});
+    }
+
+    public boolean canPathfindThrough(BlockState state, BlockView world, BlockPos pos, NavigationType type) {
+        return false;
+    }
+
+    static {
+        SHAPE = Block.createCuboidShape((double)0.0F, (double)0.0F, (double)0.0F, (double)16.0F, (double)15.0F, (double)16.0F);
+        MOISTURE = Properties.MOISTURE;
     }
 }
