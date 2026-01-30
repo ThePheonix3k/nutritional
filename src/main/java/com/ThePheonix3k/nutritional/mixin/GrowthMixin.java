44a1966d4b474e0b9632b62e9799f0be9ed4c903
ThePheonix3k <101991744+ThePheonix3k@users.noreply.github.com>
2026-01-30 13:06:07 -0500

    fixed mock mixin

--- src/main/java/com/ThePheonix3k/nutritional/mixin/GrowthMixin.java
+++ src/main/java/com/ThePheonix3k/nutritional/mixin/GrowthMixin.java
@@ -1,11 +1,15 @@
 package com.ThePheonix3k.nutritional.mixin;
 
 
+import com.ThePheonix3k.nutritional.block.ModBlocks;
 import net.minecraft.block.AttachedStemBlock;
 import net.minecraft.block.CropBlock;
 import net.minecraft.block.PitcherCropBlock;
 import net.minecraft.block.StemBlock;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import net.minecraft.block.BlockState
 
 @Mixin({PitcherCropBlock.class, CropBlock.class, AttachedStemBlock.class, StemBlock.class})
 
